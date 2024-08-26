ca614edfe433b80c3a86fb1c3f7aa777295bea25
gregoirepourtier <gpourtier@icloud.com>
2024-08-26 16:25:42 +0200

    add Ewald's method

--- src/ewald.jl
+++ src/ewald.jl
@@ -1,19 +1,38 @@
 # Ewald's Method
 
 """
-    ewald()
+    ewald(X, Y, csts)
+
+Evaluation of the Green's function using Ewald's method.
+Input arguments:
+
+  - `X`: x-coordinate of the evaluation point.
+  - `Y`: y-coordinate of the evaluation point.
+  - `csts`: tuple of constants `(a, M₁, M₂, N, β, k, d)`.
+
+Returns the value of the Green's function at the point `(X, Y)`.
 """
-function ewald()
+function ewald(X, Y, csts)
+
+    a, M₁, M₂, N, β, k, d = csts
+
+    p = 2π / d
 
     sum_1 = 0
     for m ∈ (-M₁):M₁
+        βₘ = β + m * p
+        γₘ = k <= abs(βₘ) ? √(βₘ^2 - k^2) : -im * √(k^2 - βₘ^2)
+
         sum_1 += exp(im * βₘ * Y) / (γₘ * d) * (exp(γₘ * X) * erfc(γₘ * d / (2 * a) + a * X / d) +
-                 exp(-γₘ * X) * erfc(γₘ * d / (2 * a) - a * X / d))
+                  exp(-γₘ * X) * erfc(γₘ * d / (2 * a) - a * X / d))
     end
 
     sum_2 = 0
     for m ∈ (-M₂):M₂
-        sum_2 += 1
+        rₘ = √(X^2 + (Y - m * d)^2)
+        for n ∈ 0:N
+            sum_2 += exp(im * m * β * d) * (1 / factorial(n) * (k * d / (2 * a))^(2 * n) * expint(n + 1, a^2 * rₘ^2 / d^2))
+        end
     end
 
     -1 / 4 * sum_1 - 1 / (4 * π) * sum_2
---
+++ test/test_ewald.jl
@@ -0,0 +1,21 @@
+using Pkg
+
+Pkg.activate("test/Project.toml")
+
+using Test
+using StaticArrays
+using GreenFunction
+using SpecialFunctions
+
+
+X = 0.0
+Y = 0.01 * 2π
+
+a, M₁, M₂, N, β, k, d = (2, 2, 1, 4, √2 / (2 * π), 2 / (2 * π), 2 * π)
+# a, M₁, M₂, N, β, k, d = (2, 3, 2, 7, √2 / (2 * π), 2 / (2 * π), 2 * π)
+csts = (a, M₁, M₂, N, β, k, d)
+
+# Test to match parameter from the paper (Linton, 1998)
+(X / d == 0.0, Y / d == 0.01, k * d == 2, β * d == √2)
+
+GreenFunction.ewald(X, Y, csts)

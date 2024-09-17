c5bf8e14cbe0fda067d2c39d8031679d2ce60755
gregoirepourtier <gpourtier@icloud.com>
2024-09-17 14:34:08 +0200

    push update on pkg

--- test/test_lattice_sums.jl
+++ test/test_lattice_sums.jl
@@ -4,17 +4,19 @@ Pkg.activate("test/Project.toml")
 
 using Test, GreenFunction, SpecialFunctions
 
-X, Y = (0.0, 0.01 * 2π)
+# X, Y = (0.0, 0.01 * 2π)
+X, Y = (0.0, 0.002)
 r, θ = (√(X^2 + Y^2), atan(Y, X))
 
-β, k, d, M, L = (√2 / (2 * π), 2 / (2 * π), 2 * π, 80, 4)
+# β, k, d, M, L = (√2 / (2 * π), 2 / (2 * π), 2 * π, 80, 4)
+β, k, d, M, L = (0.3, 1, 2 * π, 80, 4)
 csts = (β, k, d, M, L)
 
 # Test to match parameter from the paper (Linton, 1998)
 (X / d == 0.0, Y / d == 0.01, k * d == 2, β * d == √2, r < d)
 
 Sl = GreenFunction.lattice_sums_preparation(csts);
-eval_ls = GreenFunction.lattice_sums_calculation((X, Y), csts, Sl; nb_terms=100)
+eval_ls = -GreenFunction.lattice_sums_calculation((X, Y), csts, Sl; nb_terms=100)
 
 @test eval_ls == (-0.4595073481223693 - 0.35087920819684576 * im)
 
@@ -33,7 +35,7 @@ function test_img_exp(z, β, d; nb_terms=100)
     G
 end
 
-test_img_exp((X, Y), β, d; nb_terms=500000)
+-test_img_exp((X, Y), β, d; nb_terms=500000)
 
 function test_eigfct_exp(z, β, d; nb_terms=100)
 

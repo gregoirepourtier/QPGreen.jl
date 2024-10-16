1ae5891be6818262cef2b8cf200cdb1d64e87ebc
gregoirepourtier <gpourtier@icloud.com>
2024-10-16 14:28:52 +0200

    take constants out of loops

--- src/ewald.jl
+++ src/ewald.jl
@@ -13,26 +13,42 @@ Returns the value of the Green's function at the point `z`.
 """
 function ewald(z, csts)
 
+    # unpack various parameters for the Ewald's method 
     a, M₁, M₂, N, β, k, d = csts
 
     p = 2π / d
 
-    sum_1 = 0
+    # Initialize the sum
+    sum_M₁ = 0.0
+    sum_M₂ = 0.0
+
+    factor_1 = d / (2 * a)
+    factor_2 = a * z[1] / d
     for m ∈ (-M₁):M₁
         βₘ = β + m * p
         γₘ = k <= abs(βₘ) ? √(βₘ^2 - k^2) : -im * √(k^2 - βₘ^2)
 
-        sum_1 += exp(im * βₘ * z[2]) / (γₘ * d) * (exp(γₘ * z[1]) * SpecialFunctions.erfc(γₘ * d / (2 * a) + a * z[1] / d) +
-                  exp(-γₘ * z[1]) * SpecialFunctions.erfc(γₘ * d / (2 * a) - a * z[1] / d))
+        exp_term_1 = exp(im * βₘ * z[2]) / (γₘ * d)
+        exp_term_2 = exp(γₘ * z[1])
+
+        erfc_arg = γₘ * factor_1
+
+        sum_M₁ += exp_term_1 * (exp_term_2 * SpecialFunctions.erfc(erfc_arg + factor_2) +
+                   1 / exp_term_2 * SpecialFunctions.erfc(erfc_arg - factor_2))
     end
 
-    sum_2 = 0
+    factor_k = (k * d / (2 * a))^2
+    factor_a = a^2 / d^2
     for m ∈ (-M₂):M₂
-        rₘ = √(z[1]^2 + (z[2] - m * d)^2)
+        dₘ = m * d
+        rₘ₂ = z[1]^2 + (z[2] - dₘ)^2
+
+        exp_term = exp(im * β * dₘ)
+
         for n ∈ 0:N
-            sum_2 += exp(im * m * β * d) * (1 / factorial(n) * (k * d / (2 * a))^(2 * n) * SpecialFunctions.expint(n + 1, a^2 * rₘ^2 / d^2))
+            sum_M₂ += exp_term * (factor_k^n / factorial(n) * SpecialFunctions.expint(n + 1, factor_a * rₘ₂))
         end
     end
 
-    -1 / 4 * sum_1 - 1 / (4 * π) * sum_2
+    -1 / 4 * sum_M₁ - 1 / (4 * π) * sum_M₂
 end

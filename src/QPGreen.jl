c995971b5855c0e4acb8a86a8030806a780726f6
gregoirepourtier <gpourtier@icloud.com>
2024-12-17 17:18:13 +0100

    rename files

--- src/QPGreen.jl
+++ src/QPGreen.jl
@@ -14,19 +14,19 @@ import Bessels
 include("expansions.jl")
 export image_expansion, eigfunc_expansion
 
-include("utils.jl")
+include("fft_caches.jl")
 
 include("other_methods/lattice_sums.jl")
 include("other_methods/ewald.jl")
-include("fft_method.jl")
+include("fft_eval.jl")
 export fm_method_preparation, fm_method_calculation
 
 include("other_methods/api.jl")
 
 include("cutoff_functions.jl")
-include("fm.jl")
+include("fft_helpers.jl")
 
-include("gradient.jl")
+include("fft_gradient.jl")
 export analytical_derivative, fm_method_preparation_derivative, fm_method_calculation_derivative
 
 
